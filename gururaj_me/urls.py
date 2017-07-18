74ece95035c5001dd18cbe1e59f9f13b3280dbab
Gururaj Jeerge <gururaj.jeerge@noodle.ai>
2017-07-18 22:59:15 +0530

    Wagtail URLS configuration

--- gururaj_me/urls.py
+++ gururaj_me/urls.py
@@ -13,15 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from blogs.views import current_datetime
+
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailcore import urls as wagtail_urls
+
 
 urlpatterns = [
-	url(r'^.well-known/acme-challenge/mEun7MAQpQ2z2DgliwH0GLleKV1zK9m1jXCz5N2LL4Q$', current_datetime),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', admin.site.urls),
+    url(r'^cms/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^pages/', include(wagtail_urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
