0aebc05bcb760cbe12ca43443680d0c2c0045246
Gururaj Jeerge <gururaj.jeerge@noodle.ai>
2017-12-19 20:40:59 +0530

    Updating STATIC URL

--- gururaj_me/settings.py
+++ gururaj_me/settings.py
@@ -167,7 +167,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_ROOT = os.path.join(str(PROJECT_ROOT), 'staticfiles')
 
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(str(PROJECT_ROOT), 'media')
 
@@ -193,6 +192,8 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
+
+STATIC_URL = 'https://s3.us-east-2.amazonaws.com/iamgururaj-blog-images/'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
